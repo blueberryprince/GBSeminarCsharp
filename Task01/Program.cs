566a067eff43aa6783d1f174da692c0ddea8b45d
Maxim <makcum1298@mail.ru>
2023-08-02 17:58:17 +0300

    Task01 done

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else 
+{
+    Console.WriteLine($"Число {a} не вляется квадратом числа {b}");
+}
