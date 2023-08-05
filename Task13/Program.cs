6d402de85746a8a30f2af4755e35abbc535e49ed
Maxim <makcum1298@mail.ru>
2023-08-05 17:18:45 +0300

    tasks 10 and 13 fixed

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,13 +1,23 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите число");
-
-int num = Convert.ToInt32(Console.ReadLine());
-int check = num % 100;
-if (check == num) 
+int ThirdDigit(int num)
 {
-    Console.WriteLine("Третьей цифры нет");
-    Environment.Exit(0);
+    int check = num % 100;
+    int thirddigit = 0;
+    if (check == num)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        Environment.Exit(0);
+    }
+    else 
+    {
+       thirddigit = num % 10;
+    }
+    return thirddigit;
 }
-int ThirdDigit = num % 10;
-Console.WriteLine($"Третья цифра вашего числа - {ThirdDigit}");
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = ThirdDigit(num);
+
+Console.WriteLine($"Третья цифра вашего числа - {Math.Abs(a)}"); // На всякий случай добавил модуль числа, ибо не понравилось как с отрицательными срабатывает
