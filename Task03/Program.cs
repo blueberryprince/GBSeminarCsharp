4b1d28923f9a59a451567a20675dd2885a8c1e9d
Maxim <makcum1298@mail.ru>
2023-08-05 13:56:13 +0300

    Tasks 2-3 done

---
+++ Task03/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (a == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (a == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (a == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (a>7)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+if (a<1)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
