fa9d4d6c08fa6c46eab2cae26a8b029d48e835b0
Maxim <makcum1298@mail.ru>
2023-08-05 17:35:53 +0300

    task15 done

---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void HolidayCheck(int num)
+{
+    bool a = (num > 5) && (num < 8);
+    if (a == true)
+    {
+        Console.WriteLine("День выходной");
+    }
+    else
+    {
+        bool b = (num < 1) | (num > 7);
+        if (b == true)
+        {
+            Console.WriteLine("Вы ввели некорректное число");
+        }
+        else
+        Console.WriteLine("День не является выходным");
+    }
+}
+Console.WriteLine("Введите число, обозначающее день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+HolidayCheck(a);
