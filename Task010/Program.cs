f2c435fd6b6282a35ee2c4141fa007c2b88d08f6
Maxim <makcum1298@mail.ru>
2023-08-05 16:18:49 +0300

    task10 done

---
+++ Task010/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int RandomDigit = num % 100;
+int SecondDigit = RandomDigit / 10;
+Console.WriteLine($"Вторая цифра вашего числа - {SecondDigit}");
