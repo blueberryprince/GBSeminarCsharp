57ec2e5cb2b2c7b20e1e75c5e4d9e48671bd6a1a
Maxim <makcum1298@mail.ru>
2023-09-02 17:38:34 +0300

    Доделаны Task34 и Task36

---
+++ Task34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void ArrayCreator()
+{
+    int[] array = new int[20];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        var random = new Random();
+        array[i] = random.Next(100,1000);
+        Console.Write(array[i]+" ");
+        if (array[i] % 2 == 0) sum++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Число четных чисел в массиве - {sum}");
+}
+Console.WriteLine("Приветствую! Эта программа создает массив из 20 случайных трехзначных чисел и находит число четных чисел в нем");
+ArrayCreator();
---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void ArrayAndSummator()
+{
+    int[] array = new int[20];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        var random = new Random();
+        array[i] = random.Next(-1000,1000);
+        Console.Write(array[i]+" ");
+        if (array[i] % 2 != 0) sum = sum + array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма нечетных чисел - {sum}");
+}
+Console.WriteLine("Приветствую! Эта программа создает массив из 20 случайных трехзначных чисел и находит сумму чисел на нечетных позициях");
+ArrayAndSummator();
